e9db1f040b025834be191091ff411ba87ef2cb1b
Cameron <cameron.karasiuk@gmail.com>
2022-03-12 17:27:43 -0500

    setting number,sybol and letter variables as well as adding boxs for users to input their preferences

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 // Assignment code here
 
-//ask for passowrd length and confirm that its acceptable
-var passlen = function() {
-  var length = "";
-  
-  while (length === "" || length === null) {
-    length = prompt("how long will your password be?");
-  }
-  if (length < 8 || length > 128){
-    length = prompt("Your password length must be between 8 and 128")
-  }
+//saving possible characters as a variable
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const num = "0123456789";
+const syms = "!@#$%^&*_-+=";
+//setting generator variables
+const passwordTxt = document.getElementById("password");
+const length = document.getElementById("len");
+const incnum = document.getElementById("num");
+const incsyms = document.getElementById("syms");
 
-  console.log("the length of your password is. " + length);
-  return length;
-  
-};
+//testing varables 
+console.log(alpha)
+console.log(num)
+console.log(syms)
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtnn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-};
+//};
 
-var passinfo = {
-  length: passlen(),
-}
+//var passinfo = {
+  //length: passlen(),
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
